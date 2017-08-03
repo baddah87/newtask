b2e58317befa7a1edb63bb4085d81aa3b42b7598
baddah87 <alajmibaddah@gmail.com>
2017-08-03 19:55:07 +0300

    linking the list to the detail page

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -11,3 +12,6 @@ class Post(models.Model):
 	# slug=
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
